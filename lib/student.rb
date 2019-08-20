6192bff2e0e40337c08724828fda087b05dfd90f
David Foote <david.a.foote@gmail.com>
2019-08-20 02:44:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -73,4 +73,19 @@ class Student
       self.new_from_db(row)
     end
   end
+
+def self.students_below_12th_grade
+  sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_x_students_in_grade_10(number_of_students)
+    
+    
 end
