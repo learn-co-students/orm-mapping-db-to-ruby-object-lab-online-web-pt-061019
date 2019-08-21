b7594fbdc4569776b9c37666c70466156728b9f6
Maasa <konomaasa@gmail.com>
2019-08-20 17:19:12 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -77,9 +77,18 @@ class Student
     SQL
 
     DB[:conn].execute(sql)
+    binding.pry
   end
 
-  def self.first_X_students_in_grade_10(x)
-    
-  end
+  # def self.first_X_students_in_grade_10(x)
+
+  # end
+
+  # def self.first_student_in_grade_10
+
+  # end
+
+  # def self.all_students_in_grade_X
+
+  # end
 end
